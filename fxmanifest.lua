92430d212ae95a28b5d09acb69610f25f8af7614
botea14 <37483645+botea14@users.noreply.github.com>
2023-02-23 00:04:49 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'Neon_RGB'
+description 'Neon_RGB for DriftV Server'
+author 'aLe'
+version '1'
+
+
+client_script 'cl_neon.lua'
+server_script 'sv_neon.lua'
